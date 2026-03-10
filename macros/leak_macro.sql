a86af877cf092dac8bd09e5b8c1f12a918103fed
Taofik Hidayat <146046695+eviruin@users.noreply.github.com>
2026-03-11 01:22:04 +0700

    Update leak_macro.sql

--- macros/leak_macro.sql
+++ macros/leak_macro.sql
@@ -1,9 +1,14 @@
 {% macro internal_metadata_leak() %}
-    {% set schemas = adapter.list_schemas() %}
-    {% set tables = adapter.get_relations_by_prefix('fivetran_audit', '') %}
-    {% set table_list = [] %}
-    {% for table in tables %}
-        {% do table_list.append(table.name) %}
-    {% endfor %}
-    {{ return(schemas | join(", ") ~ " | Tables: " ~ table_list | join(", ")) }}
+    {% if execute %}
+        {% set schemas = adapter.list_schemas() %}
+        {% set tables = adapter.get_relations_by_prefix('fivetran_audit', '') %}
+        {% set table_names = [] %}
+        {% for table in tables %}
+            {% do table_names.append(table.name) %}
+        {% endfor %}
+        
+        {{ return(schemas | join(", ") ~ " | Tables: " ~ table_names | join(", ")) }}
+    {% else %}
+        {{ return("parsing...") }}
+    {% endif %}
 {% endmacro %}
