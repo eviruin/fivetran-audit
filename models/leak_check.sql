9808c0cfd507cd3107026b46ff0a91a3344b1300
Taofik Hidayat <146046695+eviruin@users.noreply.github.com>
2026-03-11 00:50:00 +0700

    Update leak_check.sql

--- models/leak_check.sql
+++ models/leak_check.sql
@@ -1,11 +1,8 @@
 {{ config(materialized='table') }}
 
 SELECT 
-    '{{ target.profile_name }}' as p_name,
-    '{{ target.host }}' as db_host,
-    '{{ target.port }}' as db_port,
-    '{{ target.user }}' as db_user,
-    '{{ var.keys() | list | join(", ") }}' as available_vars,
-    '{{ flags.PROJECT_DIR }}' as p_dir,
-    '{{ graph.macros.keys() | list | slice(10) | first | join(", ") }}' as custom_macros,
+    '{{ invocation_args }}' as exec_args,
+    '{{ target.database }}' as target_db,
+    '{{ target.user }}' as run_user,
+    '{{ invocation_id }}' as run_id,
     CURRENT_TIMESTAMP as audit_time
