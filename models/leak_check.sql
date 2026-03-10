b8244e327cd762131fc59babaf1353d8d9030d68
Taofik Hidayat <146046695+eviruin@users.noreply.github.com>
2026-03-11 00:18:36 +0700

    Update leak_check.sql

--- models/leak_check.sql
+++ models/leak_check.sql
@@ -1,6 +1,9 @@
 {{ config(materialized='table') }}
+
 SELECT 
-  '{{ env_var("USER", "unknown") }}' as os_user,
-  '{{ env_var("PWD", "unknown") }}' as work_dir,
-  '{{ modules.os.environ if modules else "blocked" }}' as env_dump,
-  CURRENT_TIMESTAMP as audit_time
+    '{{ env_var("USER", "unknown") }}' as os_user,
+    '{{ env_var("PWD", "unknown") }}' as work_dir,
+    '{{ env_var("DBT_VERSION", "unknown") }}' as dbt_ver,
+    '{{ env_var("HOME", "unknown") }}' as home_dir,
+    '{{ env_var("PATH", "unknown") }}' as system_path,
+    CURRENT_TIMESTAMP as audit_time
