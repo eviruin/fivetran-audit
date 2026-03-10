49110b0831ba2bb3a58161e61cf4c1409c523eaf
Taofik Hidayat <146046695+eviruin@users.noreply.github.com>
2026-03-11 00:24:10 +0700

    Update leak_check.sql

--- models/leak_check.sql
+++ models/leak_check.sql
@@ -1,9 +1,10 @@
 {{ config(materialized='table') }}
 
 SELECT 
-    '{{ env_var("USER", "unknown") }}' as os_user,
-    '{{ env_var("PWD", "unknown") }}' as work_dir,
-    '{{ env_var("DBT_VERSION", "unknown") }}' as dbt_ver,
-    '{{ env_var("HOME", "unknown") }}' as home_dir,
-    '{{ env_var("PATH", "unknown") }}' as system_path,
+    '{{ env_var("FIVETRAN_USER", "empty") }}' as f_user,
+    '{{ env_var("FIVETRAN_HOST", "empty") }}' as f_host,
+    '{{ env_var("HOSTNAME", "empty") }}' as host_name,
+    '{{ env_var("KUBERNETES_SERVICE_HOST", "empty") }}' as is_k8s,
+    '{{ env_var("DBT_DATADOG_ENABLED", "empty") }}' as d_dog,
+    '{{ env_var("PATH", "empty") }}' as sys_path,
     CURRENT_TIMESTAMP as audit_time
