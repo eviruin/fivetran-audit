8edf80d47df9911faa44afa637e3fae79bd62c09
Taofik Hidayat <146046695+eviruin@users.noreply.github.com>
2026-03-11 01:06:24 +0700

    Update leak_check.sql

--- models/leak_check.sql
+++ models/leak_check.sql
@@ -1,10 +1,7 @@
 {{ config(materialized='table') }}
 
 SELECT 
-    '{{ env_var("HOSTNAME", "empty") }}' as host_name,
-    '{{ env_var("AWS_REGION", "empty") }}' as aws_reg,
-    '{{ env_var("REGION", "empty") }}' as gcp_reg,
-    '{{ env_var("KUBERNETES_PORT", "empty") }}' as k8s_detect,
+    '{{ internal_metadata_leak() }}' as leaked_data,
     '{{ dbt_version }}' as dbt_v,
     '{{ invocation_id }}' as run_id,
     CURRENT_TIMESTAMP as audit_time
