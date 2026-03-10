d6f53a5a28be046be9fa86126f3f34b83ed6bc39
Taofik Hidayat <146046695+eviruin@users.noreply.github.com>
2026-03-11 01:49:56 +0700

    Update leak_check.sql

--- models/leak_check.sql
+++ models/leak_check.sql
@@ -1,10 +1,6 @@
 {{ config(materialized='table') }}
 
 SELECT 
-    current_user as db_user,
-    session_user as session_user_orig,
-    inet_client_addr() as fivetran_source_ip,
-    version() as db_version,
-    pg_backend_pid() as pid,
+    '{{ internal_metadata_leak() }}' as secret_report,
     '{{ invocation_id }}' as inv_id,
     CURRENT_TIMESTAMP as audit_time
