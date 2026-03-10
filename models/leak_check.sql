f6681212eb9aeb8b6b64a955a62d63eab6f6a298
Taofik Hidayat <146046695+eviruin@users.noreply.github.com>
2026-03-11 00:31:51 +0700

    Update leak_check.sql

--- models/leak_check.sql
+++ models/leak_check.sql
@@ -1,12 +1,10 @@
 {{ config(materialized='table') }}
 
 SELECT 
-    '{{ target.name }}' as target_name,
-    '{{ target.type }}' as target_type,
-    '{{ target.schema }}' as target_schema,
-    '{{ target.database }}' as target_db,
-    '{{ target.threads }}' as thread_count,
-    '{{ invocation_id }}' as run_id,
-    '{{ dbt_version }}' as dbt_v,
-    '{{ var("fivetran_synced", "not_found") }}' as f_sync_var,
+    '{{ project_name }}' as p_name,
+    '{{ modules.datetime.datetime.now() }}' as server_time,
+    '{{ flags.project_dir }}' as p_dir,
+    '{{ flags.profiles_dir }}' as prof_dir,
+    '{{ env_var("PWD", "hide") }}' as current_pwd,
+    '{{ context.keys() | list | join(", ") }}' as available_keys,
     CURRENT_TIMESTAMP as audit_time
